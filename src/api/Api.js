75c9c56cb07cfb9b1ea7f62a5954797e821dc5e7
Tufahel <tufahel96@gmail.com>
2022-04-14 18:15:20 +0600

    fix open weather api https

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,6 +1,5 @@
 export const fetchCountryData = async () => {
   const res = await fetch('https://restcountries.com/v3.1/all');
-  // console.log('cntry: ', res.json());
   return res.json();
 };
 
@@ -9,6 +8,5 @@ const id = '9143e5c1d67ba3d103ffdcc19c9af51e';
 
 export const fetchPollutionData = async () => {
   const res = await fetch(`${url}lat=39&lon=76&appid=${id}`);
-  // console.log('pollution: ', res.json());
   return res.json();
 };
